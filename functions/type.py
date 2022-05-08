8be70171f1a0b36380bfe98a6b7d9a03121e8967
TheoPeri <theo.perinet@epita.fr>
2022-05-08 02:21:15 +0200

    fix + add real data

--- functions/type.py
+++ functions/type.py
@@ -4,5 +4,5 @@ def delete_data_type(df):
     df.drop(columns = ["data_type"], axis =1, inplace=True)
 
 def change_data_type(df):
-    df["anomaly"] = df["data_type"] != "background"
+    df["anomaly"] = ~(df["data_type"].isin(["background", "background"]))
     df.drop(columns = ["data_type"], axis =1, inplace=True)
